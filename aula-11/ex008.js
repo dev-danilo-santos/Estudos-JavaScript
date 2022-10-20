808be2f98ebe7c303462443502fdd2fea2b12fb2
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-10-20 19:15:30 -0300

    realizada a aula 11, iniciado uma página de cadastro pessoal

---
+++ aula-11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 55
+
+if(vel > 60) {
+    console.log(`Você está a ${vel} KM/H e está acima da velocidade permitida.`)
+}
+else {
+    console.log(`Você está a ${vel} KM/H, permaneça na velocidade permitida.`)
+}
