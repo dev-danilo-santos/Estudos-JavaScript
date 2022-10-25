7b2092285202581361005607a7ae7a430ff49ae3
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-10-25 01:01:54 -0300

    finalizado o primeiro exercicio da aula 13

--- aula-13/ex-14/main.js
+++ aula-13/ex-14/main.js
@@ -6,13 +6,26 @@ function carregar(){
     var hora = data.getHours()
 
     msgHora.innerHTML = `Agora São ${hora} horas.`
-    
+    img.alt='Imagens personalizadas para a hora do dia.'
     if(hora >=0 && hora < 12 ) {
-         img.src = 'manha.png'
+         img.src = 'img/manha.png'
     } else if (hora >= 12 && hora < 18 ){
-        img.src = 'tarde.png'
+        img.src = 'img/tarde.png'
     } else {
-        img.src = 'noite.png'
+        img.src = 'img/noite.png'
     }
 
 }
+
+var txt = document.getElementById('acesso') 
+txt.addEventListener('mouseenter', mouseEntrar)
+txt.addEventListener('mouseout', mouseSair)
+
+function mouseEntrar(){ 
+    txt.style.color = 'cyan'; 
+}
+
+function mouseSair(){
+    txt.style.color = 'white'
+}
+
