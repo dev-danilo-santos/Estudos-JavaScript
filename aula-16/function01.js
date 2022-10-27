9415a1b4ca9439d133c10192177b7f3f84f56d65
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-10-27 18:55:34 -0300

    Realizando aula 16

---
+++ aula-16/function01.js
@@ -0,0 +1,10 @@
+
+function verifica (num) {
+    if(num % 2 == 0) {
+        return "Par"
+    } else {
+        return 'Ímpar'
+    }
+}
+
+console.log(verifica(9))
---
+++ aula-16/function04.js
@@ -0,0 +1,10 @@
+function fatorial( num ) {
+    let fat = 1
+    for(let i =  num; i > 1; i-- ) {
+       fat *= c
+    }
+    
+    return aux
+}
+
+console.log(fatorial(2))
